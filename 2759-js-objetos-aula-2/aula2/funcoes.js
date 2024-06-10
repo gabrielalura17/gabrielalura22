1d40653de77e30af355b959f4a5abadc6dba9bf9
gabrielalura17 <159460623+gabrielalura17@users.noreply.github.com>
2024-06-10 11:42:01 -0300

    Update funcoes.js

--- 2759-js-objetos-aula-2/aula2/funcoes.js
+++ 2759-js-objetos-aula-2/aula2/funcoes.js
@@ -1,7 +1,7 @@
 const cliente = {
-  nome: "Joao",
-  idade: 24,
-  email: "joao@firma.com",
+  nome: "Gabriel",
+  idade: 17,
+  email: "gabriel@gmail.com",
   telefone: ["1155555550", "1144444440"],
   saldo: 200,
   efetuaPagamento: function (valor) {
